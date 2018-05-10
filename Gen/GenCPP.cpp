77a376baac3209652b200f443d45849238d68042
RonxBulld <526677628@qq.com>
2018-05-10 15:28:03 +0800

    0项优化 0项修正 2项新增 【优化】 【修正】 【新增】 1. 新增自定义 structure ，必须在 module 中定义，严格按照格式 struct NAME:{} 进行定义； 2. 经过测试，FTRPC 已经参数和返回值使用 struct 类型了

--- Gen/GenCPP.cpp
+++ Gen/GenCPP.cpp
@@ -107,7 +107,8 @@ bool GenerateCPP_Provider(std::unique_ptr<RootNode> &document, TokenManage &toke
 
     FILE *pProviderHeaderFile = OpenHeadToWrite(head_file_name);
     fprintf(pProviderHeaderFile, "#define FTRPC_VERSION_MAJOR %d\n\n", document->version);
-    fprintf(pProviderHeaderFile, "\n#include <string>\n\n"
+    fprintf(pProviderHeaderFile, "\n#include <string>\n"
+                                 "#include \"json/json.h\"\n\n"
                                  "std::string ProviderDoCall(const std::string &JSON);\n\n");
     FILE *pProviderSrcFile = fopen(src_file_name, "w+");
     std::string ProviderTplFile = ReadFileAsTxt(PROVIDER_TPL_FILE);
@@ -189,7 +190,8 @@ bool GenerateCPP_Caller(std::unique_ptr<RootNode> &document, TokenManage &tokenS
 
     FILE *pCallerHeaderFile = OpenHeadToWrite(head_file_name);
     fprintf(pCallerHeaderFile, "#define FTRPC_VERSION_MAJOR %d\n\n", document->version);
-    fprintf(pCallerHeaderFile, "\n#include <string>\n\n");
+    fprintf(pCallerHeaderFile, "\n#include <string>\n"
+                               "#include \"json/json.h\"\n\n");
 
     FILE *pCallerSrcFile = fopen(src_file_name, "w+");
     std::string CallerTplFile = ReadFileAsTxt(CALLER_TPL_FILE);
